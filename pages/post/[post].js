8376c6aeec7ecfbd246e5cf8cfae7711c5333f50
Val325 <valera777pushkin777@gmail.com>
2023-08-04 20:50:51 +0300

    first commit

---
+++ pages/post/[post].js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router'
+
+export default function Post({ data }) {
+  const router = useRouter()
+  console.log("Router: ", router.query.post)
+
+  // Check if data is available and router.query.post is defined
+  let update = data && router.query.post ? Object.values(data)[router.query.post] : null;
+  console.log("Update: ", update)
+  console.log("Update typeof: ", typeof update)
+
+  return (
+    <>
+      <h1>First Post</h1>
+      {update && <p>{update.text}</p>}
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
+
+// Fetch data from external API
+export async function getServerSideProps() {
+  const res = await fetch('http://127.0.0.1:8000/api/posts/')
+  const data = await res.json()
+ 
+  // Pass data to the page via props
+  return { props: { data } }
+}
